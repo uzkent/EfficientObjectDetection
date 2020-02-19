a389df94fe990e4b00d3cf52d01151dd31d4f02e
Christopher Yeh <chrisyeh96@gmail.com>
2020-02-19 15:50:46 -0800

    fix typos; code cleanup

--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,15 @@
 import os
-import re
 import torch
 import torchvision.transforms as transforms
-import torchvision.datasets as torchdata
 import torchvision.models as torchmodels
 import numpy as np
-import pandas as pd
-import pdb
 import shutil
 import json
-import copy
 import utils_detector
-import torch.nn.functional as F
-import time
-from random import randint, sample
 
 from dataset.dataloader import CustomDatasetFromImages
 from constants import base_dir_groundtruth, base_dir_detections_cd, base_dir_detections_fd, base_dir_metric_cd, base_dir_metric_fd
-from constants import num_actions, num_windows, img_size_fd, img_size_cd
+from constants import num_windows, img_size_fd, img_size_cd
 
 def save_args(__file__, args):
     shutil.copy(os.path.basename(__file__), args.cv_dir)
@@ -30,7 +22,7 @@ def read_json(filename):
     return data
 
 def xywh2xyxy(x):
-    y = np.zeros((x.shape))
+    y = np.zeros(x.shape)
     y[:,0] = x[:, 0] - x[:, 2] / 2.
     y[:,1] = x[:, 1] - x[:, 3] / 2.
     y[:,2] = x[:, 0] + x[:, 2] / 2.
@@ -39,34 +31,34 @@ def xywh2xyxy(x):
 
 def get_detected_boxes(policy, file_dirs, metrics, set_labels):
     for index, file_dir_st in enumerate(file_dirs):
-         counter, outputs_img = 0, np.zeros((1,7))
-         for xind in range(num_windows):
-             for yind in range(num_windows):
-                 # ---------------- Read Ground Truth ----------------------------------
-                 outputs_all = []
-                 gt_path = '{}{}_{}_{}_{}_{}.txt'.format(base_dir_groundtruth, file_dir_st, str(xind), str(yind))
-                 if os.path.exists(gt_path):
-                     gt = np.loadtxt(gt_path).reshape([-1, 5])
-                     targets = np.hstack((np.zeros((gt.shape[0], 1)), gt))
-                     targets[:, 2:] = xywh2xyxy(targets[:, 2:])
-                     # ----------------- Read Detections -------------------------------
-                     if policy[index, counter] == 1:
-                         preds_dir = '{}{}_{}_{}_{}_{}'.format(base_dir_detections_fd, file_dir_st, str(xind), str(yind))
-                         targets[:, 2:] *= img_size_fd
-                         if os.path.exists(preds_dir):
-                             preds = np.loadtxt(preds_dir).reshape([-1,7])
-                             outputs_all.append(torch.from_numpy(preds))
-                     else:
-                         preds_dir = '{}{}_{}_{}_{}_{}'.format(base_dir_detections_cd, file_dir_st, str(xind), str(yind))
-                         targets[:, 2:] *= img_size_cd
-                         if os.path.exists(preds_dir):
-                             preds = np.loadtxt(preds_dir).reshape([-1,7])
-                             outputs_all.append(torch.from_numpy(preds))
-                     set_labels += targets[:, 1].tolist()
-                     metrics += utils_detector.get_batch_statistics(outputs_all, torch.from_numpy(targets), 0.5)
-                 else:
-                     continue
-                 counter += 1
+        counter = 0
+        for xind in range(num_windows):
+            for yind in range(num_windows):
+                # ---------------- Read Ground Truth ----------------------------------
+                outputs_all = []
+                gt_path = '{}{}_{}_{}_{}_{}.txt'.format(base_dir_groundtruth, file_dir_st, xind, yind)
+                if os.path.exists(gt_path):
+                    gt = np.loadtxt(gt_path).reshape([-1, 5])
+                    targets = np.hstack((np.zeros((gt.shape[0], 1)), gt))
+                    targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+                    # ----------------- Read Detections -------------------------------
+                    if policy[index, counter] == 1:
+                        preds_dir = '{}{}_{}_{}_{}_{}'.format(base_dir_detections_fd, file_dir_st, xind, yind)
+                        targets[:, 2:] *= img_size_fd
+                        if os.path.exists(preds_dir):
+                            preds = np.loadtxt(preds_dir).reshape([-1,7])
+                            outputs_all.append(torch.from_numpy(preds))
+                    else:
+                        preds_dir = '{}{}_{}_{}_{}_{}'.format(base_dir_detections_cd, file_dir_st, xind, yind)
+                        targets[:, 2:] *= img_size_cd
+                        if os.path.exists(preds_dir):
+                            preds = np.loadtxt(preds_dir).reshape([-1,7])
+                            outputs_all.append(torch.from_numpy(preds))
+                    set_labels += targets[:, 1].tolist()
+                    metrics += utils_detector.get_batch_statistics(outputs_all, torch.from_numpy(targets), 0.5)
+                else:
+                    continue
+                counter += 1
 
     return metrics, set_labels
 
@@ -94,7 +86,15 @@ def performance_stats(policies, rewards):
 
     return reward, num_unique_policy, variance, policy_set
 
-def compute_reward(offset_fd, offset_cd, reward_patch_acc, policy, beta, sigma):
+def compute_reward(offset_fd, offset_cd, policy, beta, sigma):
+    """
+    Args:
+        offset_fd: np.array, shape [batch_size, num_actions]
+        offset_cd: np.array, shape [batch_size, num_actions]
+        policy: np.array, shape [batch_size, num_actions], binary-valued (0 or 1)
+        beta: scalar
+        sigma: scalar
+    """
     # Reward function favors policies that drops patches only if the classifier
     # successfully categorizes the image
     offset_cd += beta
@@ -108,16 +108,16 @@ def get_transforms(img_size):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
     transform_train = transforms.Compose([
-       transforms.Scale(img_size),
-       transforms.RandomCrop(img_size),
-       transforms.ToTensor(),
-       transforms.Normalize(mean, std)
+        transforms.Scale(img_size),
+        transforms.RandomCrop(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
     ])
     transform_test = transforms.Compose([
-       transforms.Scale(img_size),
-       transforms.CenterCrop(img_size),
-       transforms.ToTensor(),
-       transforms.Normalize(mean, std)
+        transforms.Scale(img_size),
+        transforms.CenterCrop(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
     ])
 
     return transform_train, transform_test
